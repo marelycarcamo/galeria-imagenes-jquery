7935f81f67112d4f77adb8d553ec82c0c2b3a524
marelycarcamo <marely.carcamo@gmail.com>
2022-05-04 02:09:50 -0400

    first commit

---
+++ js/Evaluacion-ejercicio3.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function(){
+ 
+   imagen=0; 
+
+   // Añade las imagenes a la caja 
+   $("#bt-agregar").click(function(){
+      url = "imagenes/imagen" + ++imagen + ".jpg";
+      $(`<img src="${url}">`).appendTo("#imagenes");
+      imagen = imagen==10? 0:pass 
+   });
+
+
+   // Elimina la última imagen de la caja
+   $("#bt-eliminar").click(function(){
+      imagen--;
+      $("#imagenes>img:last").remove();
+      imagen = imagen<0? 0:pass
+   });
+
+
+   // Desvanece la imagen
+   $("#imagenes").on("mouseenter","img",function(){
+      $(this).fadeTo("slow",0);
+   });
+
+
+   //Reaparece la imagen desvanecida
+      $("#imagenes").on("mouseleave","img",function(){
+      $(this).fadeTo("slow",1);
+   });
+
+       
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
